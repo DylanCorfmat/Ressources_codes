42cd8e609511a008fac3392618cefd59fb1a9333
Dylan Corfmat <dylan.corfmat@gmail.com>
2019-10-24 15:10:12 +0200

    create underline effect method

---
+++ methodes/underline text effect/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dylan
+ * Date: 2019-10-24
+ * Time: 13:56
+ */
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css" />
+        <title></title>
+    </head>
+    <body>
+        <h1 class="heading">Look At This <span class="underline--magical">Pretty</span> Underline</h1>
+        <h2 class="subheading">Wow this one  is super incredibly cool, and this
+            <span class="underline--magical">one is on Multiple Lines!</span>
+            I wish I had found this like thirty projects ago when
+            I was representing the lollipop guild.</h2>
+
+        <script src="script.js"></script>
+    </body>
+</html>
